7af27c46ae0522be5bdc105e6eb108770be57797
kshahriyari <kshahriyari@isa-hamburg.com>
2019-02-09 11:17:19 +0100

    Create basic module for influencer-dashboard

---
+++ src/Influencer/Dashboard/Controller/Dashboard.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kshahriyari
+ * Date: 09.02.19
+ * Time: 11:06
+ */
+
+namespace App\Influencer\Dashboard\Controller;
+
+use App\System\Core\Helper\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use App\Influencer\UserIdValidation\Model\UserIdValidation;
+
+class Dashboard
+{
+    /**
+     * @var Request
+     */
+    protected $_request;
+
+    /**
+     * @var UserIdValidation
+     */
+    protected $_uidValidation;
+
+    public function __construct()
+    {
+        $this->_request         = new Request();
+        $this->_uidValidation   = new UserIdValidation();
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function __invoke()
+    {
+        if (empty($this->_request->getContent())) {
+            return JsonResponse::return(false);
+        }
+
+        $requestJson = json_decode($this->_request->getContent(), true);
+
+        $uid = $requestJson['uid'];
+
+
+        $validate = $this->_uidValidation->validateUniqueUserId($uid);
+        if($validate){
+            return JsonResponse::return(true);
+        }
+
+        return JsonResponse::return(false);
+
+    }
+
+}
